ddaacd5be99726b3af7dbaff49defb72cafbf3ca
micaro <mika.ropponen@gmail.com>
2016-09-10 07:28:04 +0300

    first commit

---
+++ passi-rest/src/main/java/fi/softala/ttl/configuration/ApplicationConfiguration.java
@@ -0,0 +1,13 @@
+package fi.softala.ttl.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "fi.softala.ttl.*")
+public class ApplicationConfiguration {
+	
+
+}
---
+++ passi-rest/src/main/java/fi/softala/ttl/service/UserService.java
@@ -0,0 +1,27 @@
+package fi.softala.ttl.service;
+
+import java.util.List;
+
+import fi.softala.ttl.model.User;
+
+
+
+public interface UserService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers(); 
+	
+	void deleteAllUsers();
+	
+	public boolean isUserExist(User user);
+	
+}
